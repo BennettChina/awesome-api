c445616e0f9f1c5ba73d2e7c06cb232825eaf7db
bennett <bennett_asia@outlook.com>
2024-08-22 10:59:55 +0800

    fix: 修改接口参数

--- api/model/captcha.py
+++ api/model/captcha.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel
 
 
 class Captcha(BaseModel):
-    gt: str
     geetest_challenge: str
     geetest_validate: str
     geetest_seccode: str
--- api/router/captcha.py
+++ api/router/captcha.py
@@ -1,11 +1,8 @@
-import hashlib
-
 from fastapi import APIRouter
 
 from api.model.captcha import Captcha
 from api.model.resp import ok, error
 from modules.redis_client import redis_client
-from utils.logger import logger
 
 router = APIRouter()
 
@@ -17,27 +14,20 @@ async def captcha(item: Captcha):
     :param item: 验证码通过的结果
     :return: None
     """
-    key = f"{item.gt}:{item.geetest_challenge}"
-    key = hashlib.md5(key.encode()).hexdigest()
-    logger.info(f'key: {key}')
-    redis_client.hset_all(key, item.__dict__)
+    redis_client.hset_all(item.geetest_challenge, item.__dict__)
     # 缓存验证结果 10 分钟
-    redis_client.expire(key, 600)
+    redis_client.expire(item.geetest_challenge, 600)
     return ok()
 
 
 @router.get("/manual/captcha", tags=["captcha"])
-async def captcha_get(gt: str, challenge: str):
+async def captcha_get(challenge: str):
     """
     获取验证码的结果
-    :param gt: gt
     :param challenge: challenge
     :return: see Captcha class
     """
-    key = f"{gt}:{challenge}"
-    key = hashlib.md5(key.encode()).hexdigest()
-    logger.info(f'key: {key}')
-    data = redis_client.hget_all(key)
+    data = redis_client.hget_all(challenge)
     if data.__len__() == 0:
         return error(1401, "invalid params")
     return ok(data)
